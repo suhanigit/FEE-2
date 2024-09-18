3b1a0af3718b1ce4629a99beaa688927a5c4f989
suhani <suhani8542@gmail.com>
2024-09-18 12:55:59 +0530

    Objects In javascript

---
+++ Javascript/objects.js
@@ -0,0 +1,67 @@
+//OBJECT-stores complex data
+//OBJECT CAN BE DECLARED BY TWO METHODS--> Constructor and by literals
+
+//constructor
+// var obj1= new Object(value);
+// obj1.name ="Shivam"
+// console.log(obj1);
+
+
+// const obj = new Object();
+// obj.name="Shivang";
+// console.log(obj.name);
+
+
+
+//Literals
+// var obj={
+//     name: "yash",
+//     class: "FEE",
+//     RollNo: 25,
+// };
+// console.log(obj.name);
+
+
+// const users ={
+//     name:"kanav",
+//     'full name': "Kanav Aggarwal",
+//     age: 25,
+//     subject: "FEE",
+//     CGPA: 8.6,
+// };
+// console.log(typeof users);
+// console.log(users.name);
+// console.log(users["name"]);
+// console.log(users['full name']);
+
+
+
+//Changing or updating object value
+// users.name ="Mohit";
+// console.log(users);
+
+
+//object inside an object
+// const users ={
+//     user1:{
+//         name: "yash",
+//         age: 25,
+//     },
+//     user2:{
+//         name: {
+//            income: 20000, 
+//            rollNo: 2,
+//         },
+//         age: 26,
+//     },
+//     user3:{
+//         name: "Shubham",
+//         age: 24,
+//     }
+// }
+// console.log(users.user2.name.income);
+// console.log(users.user2.name);
+
+
+
+//LITERALS
